dbceb49895a4d18a39396fea0534ac0a5a5f1a21
ize12 <b2nagkor@vasvari.org>
2026-09-11 11:52:49 +0200

    Orai_setup_feladat

---
+++ React_Orai/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  const nev: string = "Kornél";
+
+  return (
+    <>
+      <h1>szoveg</h1>
+      <p>Az én nevem {nev}</p>
+    </>
+  );
+};
+
+export default App;
