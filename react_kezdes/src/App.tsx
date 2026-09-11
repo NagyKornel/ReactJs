67f37ad3122567fed9419c547400b894cbdf08cd
ize12 <b2nagkor@vasvari.org>
2026-09-11 11:33:53 +0200

    szoveg

--- react_kezdes/src/App.tsx
+++ react_kezdes/src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 
 const App = () => {
+  const nev: string = "Pisti";
   return (
     <>
       <h1>szoveg</h1>
-      <p>szoveg</p>
+      <p>
+        Az én nevem: <b>{nev}</b>
+      </p>
     </>
   );
 };
