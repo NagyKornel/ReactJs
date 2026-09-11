c9694467ea5135459bed4d80093a0c51d5a903ef
ize12 <b2nagkor@vasvari.org>
2026-09-11 12:17:50 +0200

    tablazat keszites

--- React_Orai/src/App.tsx
+++ React_Orai/src/App.tsx
@@ -1,12 +1,37 @@
 import "./App.css";
+import type { Ember } from "./types/ember";
 
 const App = () => {
-  const nev: string = "Kornél";
+  const emberek: Array<Ember> = [
+    { nev: "John Doe", kor: 30, varos: "New York" },
+    { nev: "Jane Smith", kor: 25, varos: "Los Angeles" },
+    { nev: "Alice Jhonson", kor: 28, varos: "Chicago" },
+  ];
+
+  emberek.push({ nev: "x", kor: 15, varos: "Szeged" } as Ember);
+
+  const generateRow = (e: Ember) => {
+    return (
+      <tr>
+        <td>{e.nev}</td>
+        <td>{e.kor}</td>
+        <td>{e.varos}</td>
+      </tr>
+    );
+  };
 
   return (
     <>
-      <h1>szoveg</h1>
-      <p>Az én nevem {nev}</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Név</th>
+            <th>Kor</th>
+            <th>Város</th>
+          </tr>
+        </thead>
+        <tbody>{emberek.map((i) => generateRow(i))}</tbody>
+      </table>
     </>
   );
 };
