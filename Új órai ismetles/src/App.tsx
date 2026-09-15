8817fee3ec4cdeba1b1c5e015fe80d1c83a2194b
ize12 <b2nagkor@vasvari.org>
2026-09-15 10:02:06 +0200

    új órai

---
+++ Új órai ismetles/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  const name: string = "Kornél";
+
+  return (
+    <>
+      <h1>Hello!</h1>
+      <h2>A nevem {name}</h2>
+    </>
+  );
+}
+
+export default App;
