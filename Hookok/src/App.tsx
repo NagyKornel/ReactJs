55ab4d2f2f74d5b0de91124589910be06419e67c
ize12 <b2nagkor@vasvari.org>
2026-09-17 10:39:18 +0200

    szinek

--- Hookok/src/App.tsx
+++ Hookok/src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import Bemutatkozas from "./Bemutatkozas";
 
-function App() {
+const App = () => {
+  const kor = 20;
+
   return (
     <>
       <h1>Hello World!</h1>
-      <Bemutatkozas nev={"Valaki"} kor={15} />
+      <Bemutatkozas nev={"Valaki"} kor={kor} />
     </>
   );
-}
+};
 
 export default App;
--- Hookok/src/Bemutatkozas.tsx
+++ Hookok/src/Bemutatkozas.tsx
@@ -8,6 +8,11 @@ const Bemutatkozas = (props: Props) => {
     <>
       <h1>Hello {props.nev}!</h1>
       <h2>te {props.kor} éves vagy!</h2>
+      {props.kor < 18 ? (
+        <p style={{ color: "red" }}>Te fiatalkorú vagy!</p>
+      ) : (
+        <p style={{ color: "green" }}>Te felnőtt vagy!</p>
+      )}
     </>
   );
 };
