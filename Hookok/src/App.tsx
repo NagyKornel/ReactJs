c455ef583f5425b07ea454676152f3390a9a62f9
ize12 <b2nagkor@vasvari.org>
2026-09-17 10:28:13 +0200

    Proppok

---
+++ Hookok/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Bemutatkozas from "./Bemutatkozas";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Bemutatkozas nev={"Valaki"} kor={15} />
+    </>
+  );
+}
+
+export default App;
