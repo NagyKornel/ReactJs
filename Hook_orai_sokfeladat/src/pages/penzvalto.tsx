f7bb328f45c3ceda27b6874f5b44349f579b5ef6
Nagy Kornel <b2nagkor@vasvari.org>
2026-09-18 15:27:49 +0200

    penzvalto

--- Hook_orai_sokfeladat/src/pages/penzvalto.tsx
+++ Hook_orai_sokfeladat/src/pages/penzvalto.tsx
@@ -1,4 +1,31 @@
+import { useState } from "react";
+
 const penzvalto = () => {
-  return <></>;
+  const [forint, setForint] = useState<number>(0);
+  const [valuta, setValue] = useState<string>("usd");
+  const [eredmeny, setEredmeny] = useState<number>(0);
+  return (
+    <>
+      <input
+        onChange={(e) => setForint(Number(e.target.value))}
+        type="number"
+        placeholder="500"
+      />
+      <select onChange={(v) => setValue(v.target.value)}>
+        <option value="usd">Dollár</option>
+        <option value="eur">Euró</option>
+      </select>
+
+      <button
+        onClick={() => {
+          if (valuta == "usd") setEredmeny(forint / 350);
+          else if (valuta == "eur") setEredmeny(forint / 380);
+        }}
+      >
+        Számítás
+      </button>
+      <p>{eredmeny.toFixed(2)}</p>
+    </>
+  );
 };
 export default penzvalto;
