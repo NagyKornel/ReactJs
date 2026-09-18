6ae31c010bd3d141c47c904a6b6dde07be2a3589
ize12 <b2nagkor@vasvari.org>
2026-09-18 09:15:53 +0200

    gombok

--- Hookok/src/App.tsx
+++ Hookok/src/App.tsx
@@ -1,15 +1,32 @@
 import "./App.css";
-import Bemutatkozas from "./Bemutatkozas";
 
-const App = () => {
-  const kor = 20;
+function App() {
+  const submit = () => {
+    alert("Megnyomva3!");
+  };
+  const submit2 = (szoveg: string) => {
+    alert(szoveg);
+  };
 
   return (
     <>
-      <h1>Hello World!</h1>
-      <Bemutatkozas nev={"Valaki"} kor={kor} id={1} />
+      <h1>Hello World</h1>
+      <button onClick={() => alert("Megnyomva!")}>Nyomj meg1</button>
+      {/* ha egysoros akk nem kell genyo zarojel */}
+      <button
+        onClick={() => {
+          // de lehet rakni
+          alert("Megnyomva2!");
+        }}
+      >
+        Nyomj meg2
+      </button>
+      <button onClick={() => submit()}>Nyomj meg3</button>
+      <button onClick={submit}>Nyomj meg4</button>
+      {/* nem kell az a mocskos sok genyo oda mert nincs paraméter */}
+      <button onClick={() => submit2("valami")}>Nyomj meg5</button>
     </>
   );
-};
+}
 
 export default App;
