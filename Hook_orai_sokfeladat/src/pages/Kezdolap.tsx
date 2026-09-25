30df0cef8021f03d041c93d204ac19716ee223f0
ize12 <b2nagkor@vasvari.org>
2026-09-25 11:33:01 +0200

    kezdolap elkeszitese

--- Hook_orai_sokfeladat/src/pages/Kezdolap.tsx
+++ Hook_orai_sokfeladat/src/pages/Kezdolap.tsx
@@ -1,7 +1,23 @@
 const Kezdolap = () => {
   return (
     <>
-      <h1>Ez itt a kezdőlap</h1>;
+      <h1>Ez itt a kezdőlap</h1>
+      <button>
+        <a href="/szamologep">Számológépre</a>
+      </button>
+      <br />
+      <button>
+        <a href="/Penzvalto">Pénzváltóra</a>
+      </button>
+      <br />
+      <button>
+        <a href="/Bmi">BMI-re</a>
+      </button>
+      <br />
+      <button>
+        <a href="/Homerseklet">Hőmérséklet átváltóra</a>
+      </button>
+      <br />
     </>
   );
 };
