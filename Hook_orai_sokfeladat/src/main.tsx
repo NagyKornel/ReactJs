2bb528dc07a2cf93010d9c808a564fea0e1347f1
ize12 <b2nagkor@vasvari.org>
2026-09-25 09:36:13 +0200

    Formazas

--- Hook_orai_sokfeladat/src/main.tsx
+++ Hook_orai_sokfeladat/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Penzvalto from "./pages/penzvalto";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Kezdolap from "./pages/Kezdolap";
 import Szamologep from "./pages/szamologep";
+import Penzvalto from "./pages/penzvalto";
 import Bmi from "./pages/bmikalk";
 import Homerseklet from "./pages/homerseklet";
 import Notfound from "./pages/Notfound";
